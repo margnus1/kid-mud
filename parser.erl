1a576d1c918c1eed389c94c38fb853964175d227
Magnus Lång <margnus1@telia.com>
2012-04-26 23:18:46 +0200

    Added parser unit tests

--- parser.erl
+++ parser.erl
@@ -1,5 +1,6 @@
 -module(parser).
 -export([parse/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 %% @doc Parses a user command string into a usable representation
 parse(String) ->
@@ -9,3 +10,16 @@ parse(String) ->
         Atom when is_atom(Atom) -> Atom;
         _ErrorMessage -> parse_error
     end.
+
+
+parse_test_() ->
+    [?_assertEqual(parse_error, parse("")),
+     ?_assertEqual(logout     , parse("logout")),
+     ?_assertEqual(look       , parse("look")),
+     ?_assertEqual(look       , parse("l")),
+     ?_assertEqual(parse_error, parse("lo")),
+     ?_assertEqual({go, north}, parse("go north")),
+     ?_assertEqual({go, east} , parse("move e")),
+     ?_assertEqual({go, west} , parse("west")),
+     ?_assertEqual({go, south}, parse("s")),
+     ?_assertEqual(parse_error, parse("go southish"))].
