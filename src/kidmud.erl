44e04329179cd9815c2cd93d96eedcbd5d2fa95b
Magnus <margnus1@telia.com>
2012-05-09 14:05:09 +0200

    Added a master supervisor

---
+++ src/kidmud.erl
@@ -0,0 +1,84 @@
+%%%-------------------------------------------------------------------
+%%% @author Magnus Lång <mala7837@beurling.it.uu.se>
+%%% @copyright (C) 2012, Magnus Lang
+%%% @doc
+%%%      Main supervisor for the Kid-MUD server
+%%% @end
+%%% Created :  9 May 2012 by Magnus Laåg <mala7837@beurling.it.uu.se>
+%%%-------------------------------------------------------------------
+-module(kidmud).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor whithout a link.
+%% To be used from shell when debugging only.
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start() ->
+    {ok, Pid} = supervisor:start_link({local, ?SERVER}, ?MODULE, []),
+    unlink(Pid).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000, % timeout
+    Type = worker,
+
+    database:start(),
+
+    Zonemaster = {"Zone master", {zonemaster, start_link, []},
+		  Restart, Shutdown, worker, [zonemaster]},
+
+    {ok, {SupFlags, [Zonemaster]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
