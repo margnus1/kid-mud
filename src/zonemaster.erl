52fd4786dcc275ae9a86d7e311d0892effbfbfb7
Magnus <margnus1@telia.com>
2012-05-13 18:19:34 +0200

    Fixed problem with context setup in zonemaster tests

--- src/zonemaster.erl
+++ src/zonemaster.erl
@@ -211,6 +211,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 test_setup() ->
     mnesia:start(),
+    database:create_tables([]),
     database:write_zone(#zone{id=1234}),    
     database:write_zone(#zone{id=1235}),
     start_link(),
